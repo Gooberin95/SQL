e6f779329d4b3ca24caa888ffb37b7554e24cd20
Adonis95 <Sober4202020@outlook.com>
2025-04-01 21:15:57 -0600

    testing out sql statements

--- thing.py
+++ thing.py
@@ -49,3 +49,19 @@ def test_dist():
 
 # test_dist()
 
+def order_by_desc():
+    try:
+        engine = create_engine(connection_string)
+        with engine.connect() as connection:
+            query = "SELECT [Work/Study Hours], [Financial Stress] FROM Homes ORDER BY [Work/Study Hours] DESC, [Financial Stress] DESC"
+
+            df = pd.read_sql(query, connection)
+            print(df)
+            print('Success')
+    except Exception as e:
+        print(f"An error occurred, {e}")
+    
+
+order_by_desc()
+
+
